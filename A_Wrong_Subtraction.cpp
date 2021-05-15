4ae7eccf5d8fad8ebb0415f353accb369199d3e3
YASHVI BANSAL <61915754+yashvibansalmin19@users.noreply.github.com>
2021-05-15 16:43:12 +0530

    new

--- A_Wrong_Subtraction.cpp
+++ A_Wrong_Subtraction.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
 int main(){
-    
+    ios_base:: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int a,b;
+    cin>>a>>b;
+    while (b!=0){
+       if(a%10==0){
+           a/=10;
+       }
+       else{
+           a-=1;
+       }
+        b--;
+    }
+    cout<<a;
     return 0;
 }
