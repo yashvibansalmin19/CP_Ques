706f891492990376b7d4f01233337f7f96dfa8f1
YASHVI BANSAL <61915754+yashvibansalmin19@users.noreply.github.com>
2021-05-21 13:53:41 +0530

    ContestDIV2_April03_2018

---
+++ A_Mahmoud_and_Ehab_and_the_even_odd_game.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    ios_base:: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n;
+    cin>>n;
+    int i=1;
+    if(n%2!=0){
+        cout<<"Ehab";
+    }
+    else{
+        cout<<"Mahmoud";
+    } 
+return 0;
+}
